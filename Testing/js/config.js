54c67006be59aa3e7e98f2b3b8efb4c5166c124a
StGreg13 <gregory.stepanov@live.ru>
2019-06-24 22:10:52 +0300

    rename click

--- Testing/js/config.js
+++ Testing/js/config.js
@@ -1,23 +1,24 @@
-$(function(){ 
-    $('[placeholder]').placeholder();    
-});  
+$(function() {
+  $("[placeholder]").placeholder();
+});
 
 $(function() {
-    $('a[href^="#"]').click(function (){
-        var elementClick = $(this).attr("href");
-        var destination = $(elementClick).offset().top - 60;
-        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
-        return false;
-    });  
+  $('a[href^="#"]').click(function() {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top - 60;
+    jQuery("html:not(:animated), body:not(:animated)").animate(
+      { scrollTop: destination },
+      800
+    );
+    return false;
+  });
 
-   $('.slider').slick({
-  dots: false,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  nav:false,
-  cssEase: 'linear',
-  
+  $(".slider").click({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    nav: false,
+    cssEase: "linear"
+  });
 });
-});  
-
