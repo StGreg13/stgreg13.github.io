659c325e8a13225d3d2ad4e2bbe755afb40e6742
StGreg13 <gregory.stepanov@live.ru>
2020-05-26 00:33:28 +0300

    adaptive added

--- VKrai/js/carousel.js
+++ VKrai/js/carousel.js
@@ -53,4 +53,22 @@ $(".owl-carousel").owlCarousel({
   nav: true,
   navText: ["", ""],
   rewindNav: true,
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 2,
+      nav: true,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: true,
+    },
+  },
 });
